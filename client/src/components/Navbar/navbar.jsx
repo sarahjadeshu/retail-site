84d8df8daa37f8d8d3135214736561daad4ee3ee
Sarah Shu <sarahjadeshu@gmail.com>
2023-10-13 00:15:00 -0700

    added mui icons

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Navbar = () => {
   return (
