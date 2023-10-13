a914f8edecb5afec413fbfa5dd4b34d9f115cefc
Sarah Shu <sarahjadeshu@gmail.com>
2023-10-12 21:23:37 -0700

    updated navbar component and installed sass

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left"></div>
+        <div className="center"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
