0b9b403fce995bbe2d71c963baed48533d57b97d
Sarah Shu <sarahjadeshu@gmail.com>
2023-10-13 17:03:03 -0700

    modified navbar component

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
@@ -14,8 +15,23 @@ const Navbar = () => {
             <img src="/images/english.png" alt="Flag for English language" />
             <KeyboardArrowDownIcon />
           </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/3">Children</Link>
+          </div>
         </div>
-        <div className="center"></div>
+        <div className="center">
+          <Link to="/">ECHO SHOP</Link>
+        </div>
         <div className="right"></div>
       </div>
     </div>
