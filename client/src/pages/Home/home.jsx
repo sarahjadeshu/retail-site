766670b9ab47e8af986fbe3e4d4ea5428c820abc
Sarah Shu <sarahjadeshu@gmail.com>
2023-10-17 15:28:31 -0700

    added featured products component

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Slider from "../../components/Slider/slider";
+import Featured from "../../components/Featured/featured";
 import "./home.scss"
 
 const Home = () => {
     return (
       <div className="home">
         <Slider />
+        <Featured type="featured" />
+        <Featured type="trending" />
       </div>
     );
 };
