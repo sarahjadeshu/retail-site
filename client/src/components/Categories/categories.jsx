a639425430d9c815e8c9bd43f7a6f345823e6ae3
Sarah Shu <sarahjadeshu@gmail.com>
2023-10-20 18:45:44 -0700

    created grid display on home page categories component

--- client/src/components/Categories/categories.jsx
+++ client/src/components/Categories/categories.jsx
@@ -12,24 +12,76 @@ const Categories = () => {
               alt="Person wearing yellow coat holding pink flower"
             />
             <button>
-                <Link className="link" to="/products/1">Sale</Link>
+              <Link className="link" to="/products/1">
+                Sale
+              </Link>
             </button>
           </div>
-          <div className="row">r2</div>
+          <div className="row">
+            <img
+              src="https://images.pexels.com/photos/3328128/pexels-photo-3328128.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Khaki robe and brown plaid pants"
+            />
+            <button>
+              <Link className="link" to="/products/1">
+                Women
+              </Link>
+            </button>
+          </div>
         </div>
         <div className="col">
-          <div className="row">r3</div>
+          <div className="row">
+            <img
+              src="https://images.pexels.com/photos/3419692/pexels-photo-3419692.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Woman wearing pleated silver dress celebrating with confetti and balloons"
+            />
+            <button>
+              <Link className="link" to="/products/1">
+                New Season
+              </Link>
+            </button>
+          </div>
         </div>
         <div className="col col-l">
           <div className="row">
             <div className="col">
-              <div className="row">r4</div>
+              <div className="row">
+                <img
+                  src="https://images.pexels.com/photos/2899935/pexels-photo-2899935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt="Man standing in long grass wearing black and white contrast blazer coat"
+                />
+                <button>
+                  <Link className="link" to="/products/1">
+                    Men
+                  </Link>
+                </button>
+              </div>
             </div>
             <div className="col">
-              <div className="row">r5</div>
+              <div className="row">
+                <img
+                  src="https://images.pexels.com/photos/6624862/pexels-photo-6624862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt="Gold necklaces surrounded by beige fabric and pink flowers"
+                />
+                <button>
+                  <Link className="link" to="/products/1">
+                    Accessories
+                  </Link>
+                </button>
+              </div>
             </div>
           </div>
-          <div className="row">r6</div>
+          <div className="row">
+            <img
+              src="https://images.pexels.com/photos/12750409/pexels-photo-12750409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Person wearing black polka dot dress holding green pointed toe heels"
+            />
+            <button>
+              <Link className="link" to="/products/1">
+                Shoes
+              </Link>
+            </button>
+          </div>
         </div>
       </div>
     );
