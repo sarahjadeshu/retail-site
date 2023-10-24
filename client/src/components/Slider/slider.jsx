65457ade3546952af1b3f92e9d458c8865bdde92
Sarah Shu <sarahjadeshu@gmail.com>
2023-10-24 01:29:30 -0700

    updated color scheme

--- client/src/components/Slider/slider.jsx
+++ client/src/components/Slider/slider.jsx
@@ -8,9 +8,9 @@ const Slider = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
 
     const data = [
-      "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      "https://images.pexels.com/photos/7825934/pexels-photo-7825934.jpeg",
+      "https://images.pexels.com/photos/15791710/pexels-photo-15791710/free-photo-of-model-wearing-necklace.jpeg",
+      "https://images.pexels.com/photos/9818830/pexels-photo-9818830.jpeg",
     ];
 
     const prevSlide = () => {
--- client/src/pages/Product/product.jsx
+++ client/src/pages/Product/product.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import "./product.scss";
 
 const Product = () => {
-  return <div className="product">Product</div>;
+
+  const [selectedImg, setSelectedImg] = useState(0);
+
+  const images = [
+    "https://images.pexels.com/photos/18829290/pexels-photo-18829290/free-photo-of-a-woman-in-an-orange-coat-and-white-pants.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/18696169/pexels-photo-18696169/free-photo-of-a-woman-in-a-green-dress-stands-on-a-balcony.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
+
+  return <div className="product">
+    <div className="left">
+      <div className="images">
+        <img src={images[0]} alt="" onClick={e => setSelectedImg(0)} />
+        <img src={images[1]} alt="" onClick={e => setSelectedImg(1)} />
+      </div>
+      <div className="mainImg">
+        <img src={images[selectedImg]} alt="" />
+      </div>
+    </div>
+    <div className="right"></div>
+  </div>;
 };
 
 export default Product;
