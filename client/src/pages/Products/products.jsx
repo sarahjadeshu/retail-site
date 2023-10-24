deb22fdebbdd691ddf5bcee59e3f7a07f9099048
Sarah Shu <sarahjadeshu@gmail.com>
2023-10-23 21:12:44 -0700

    updated products page with sort and filter functionality

--- client/src/pages/Products/products.jsx
+++ client/src/pages/Products/products.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import List from "../../components/List/list";
 import "./products.scss";
 
 const Products = () => {
+
+const catId = parseInt(useParams().id);
+const [maxPrice, setMaxPrice] = useState(1000);
+const [sort, setSort] = useState(null);
+
   return (
     <div className="products">
       <div className="left">
@@ -24,19 +32,19 @@ const Products = () => {
           <h2>Filter By Price</h2>
           <div className="inputItem">
             <span>0</span>
-            <input type="range" min={0} max={1000} />
-            <span>1000</span>
+            <input type="range" min={0} max={1000} onChange={(e) => setMaxPrice(e.target.value)}/>
+            <span>{maxPrice}</span>
           </div>
         </div>
         <div className="filterItem">
           <h2>Sort By</h2>
           <div className="inputItem">
-            <input type="radio" id="asc" value="asc" name="price" />
+            <input type="radio" id="asc" value="asc" name="price" onChange={e => setSort("asc")}/>
             <label htmlFor="asc">Price (Low to high)</label>
           </div>
           <div className="inputItem">
-            <input type="radio" id="asc" value="asc" name="price" />
-            <label htmlFor="asc">Price (High to low)</label>
+            <input type="radio" id="desc" value="desc" name="price" onChange={e => setSort("desc")}/>
+            <label htmlFor="desc">Price (High to low)</label>
           </div>
         </div>
       </div>
@@ -46,7 +54,7 @@ const Products = () => {
           src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
           alt="Person sitting on stone bench, looking towards the ocean and a lighthouse"
         />
-        
+        <List catId={catId} maxPrice={maxPrice} sort={sort} />
       </div>
     </div>
   );
