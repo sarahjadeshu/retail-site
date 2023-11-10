c997c389e252ce7972f86d6887cf10119b1cd81c
Sarah Shu <sarahjadeshu@gmail.com>
2023-11-09 21:58:30 -0800

    implemented filter by price for products

--- client/src/components/List/list.jsx
+++ client/src/components/List/list.jsx
@@ -6,7 +6,9 @@ import useFetch from "../../hooks/useFetch";
 const List = ({ subCats, maxPrice, sort, catId }) => {
 
     const { data, loading, error } = useFetch(
-      `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(item => `&[filters][sub_categories][id][$eq]=${item}`)}`
+      `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+        item => `&[filters][sub_categories][id][$eq]=${item}`
+        )}&[filters][price][$lte]=${maxPrice}`
     );
 
     return (
