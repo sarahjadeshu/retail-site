2e0ed4c40815c9104694fd9002da72c86d07c58a
Sarah Shu <sarahjadeshu@gmail.com>
2024-01-22 16:07:13 -0800

    updated about page

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -3,12 +3,13 @@ import "./about.scss";
 
 const About = () => {
     return (
-        <div className="about">
-            <div className="title">
-                <h1>About Us</h1>
-            </div>
+      <div className="about">
+        <div className="title">
+          <h1>About Us</h1>
+          <img src="/images/about-display.jpeg" alt="Boutique with clothing rack" />
         </div>
-    )
+      </div>
+    );
 }
 
 export default About;
